e2e2e28fa6726f11bab46cbe48ce525128fd91b4
Vijay Pratap Singh <vijaypratap12121999@gmail.com>
2023-06-30 15:16:14 +0530

    added a new post endpoint

---
+++ PracticeCICDGithub/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PracticeCICDGithub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        /// <summary>
+        /// This method is used to get values. 
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult GetValues()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// This method is used to post values data.
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult GetData()
+        {
+            return Ok();
+        }
+    }
+}
